198dad01ac56b141bab473acd35f16bdef742bee
unknown <piotrasmc@gmail.com>
2021-07-02 01:01:12 +0200

    Drag & drop

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,42 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+ <div>
+    <div 
+            class='drop-zone'  
+            @drop='onDrop($event, 1)'       
+            @dragover.prevent
+            @dragenter.prevent
+            >
+<!--       <div v-for='item in listOne' :key='item.title' class='drag-el'> -->
+            <div 
+                    class='drag-el' 
+                    v-for='item in listOne' 
+                    :key='item.title' 
+                    draggable
+                    @dragstart='startDrag($event, item)'
+            >
+                    {{ item.title }}
+            </div>
+<!--       </div> -->
+    </div>
+    <div 
+            class='drop-zone' 
+            @drop='onDrop($event, 2)'       
+            @dragover.prevent
+            @dragenter.prevent
+        
+        >
+<!--       <div v-for='item in listTwo' :key='item.title' class='drag-el'> -->
+            <div 
+                    class='drag-el' 
+                    v-for='item in listTwo' 
+                    :key='item.title' 
+                    draggable
+                    @dragstart='startDrag($event, item)'
+            >
+                    {{ item.title }}
+            </div>
+<!--       </div> -->
+    </div>
   </div>
 </template>
 
@@ -37,24 +45,63 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  data: () => ({
+      items: [
+      {
+        id: 0,
+        title: 'Item A',
+        list: 1
+      },
+      {
+        id: 1,
+        title: 'Item B',
+        list: 1
+      },
+      {
+        id: 2,
+        title: 'Item C',
+        list: 2
+      }]
+  
+    }),
+    
+    computed: {
+        listOne () {
+        return this.items.filter(item => item.list === 1)
+        },
+        listTwo () {
+        return this.items.filter(item => item.list === 2)
+        }
+    }  ,
+    methods : {
+        startDrag: (evt, item) => {
+            evt.dataTransfer.dropEffect = 'move'
+            evt.dataTransfer.effectAllowed = 'move'
+            evt.dataTransfer.setData('itemID', item.id)
+        },
+        onDrop (evt, list) {
+            const itemID = evt.dataTransfer.getData('itemID')
+            const item = this.items.find(item => item.id == itemID)
+            item.list = list
+        }        
+    
+    },
+    
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .drop-zone {
+    background-color: #eee;
+    margin-bottom: 10px;
+    padding: 10px;
+  }
+
+  .drag-el {
+    background-color: #fff;
+    margin-bottom: 10px;
+    padding: 5px;
+  }
 </style>
